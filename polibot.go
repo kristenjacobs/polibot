147040072c7a953b6748923323fca80d8d1072ad
kris <kristen.f.jacobs@oracle.com>
2017-05-25 14:04:45 +0100

    More updates..

--- polibot.go
+++ polibot.go
@@ -6,13 +6,18 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func GetResponseString() string {
 	acceptableResponse := []string{
+		"Given this is an ongoing investigation, I am unable comment on specific details. However, what I can say is that, as a party, we will do everything in our power to ensure that this issue get resolved in both a quick and decisive manner.",
 		"What we need is a full and frank discussion about the issues that really matter",
 		"Lets be clear, nothing has changed in our manifesto, merely a clarification of the details",
 		"What this country deserves is a strong and stable leadership",
 	}
-	fmt.Fprintf(w, "%s\n", acceptableResponse[rand.Intn(len(acceptableResponse))])
+	return acceptableResponse[rand.Intn(len(acceptableResponse))]
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\n", GetResponseString())
 }
 
 func main() {
---
+++ polibot_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetResponseString(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		result := GetResponseString()
+		if len(result) < 1 {
+			t.Error("Failed to return a valid response")
+		}
+	}
+}
