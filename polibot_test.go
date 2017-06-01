eec728339e01833a303686480c200c75dffe80d5
kris <kristen.f.jacobs@oracle.com>
2017-06-01 16:06:26 +0100

    Fixed failing test

--- polibot_test.go
+++ polibot_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGetResponseString(t *testing.T) {
 	for i := 0; i < 10; i++ {
-		result := GetResponseString()
+		result := getResponseString()
 		if len(result) < 1 {
 			t.Error("Failed to return a valid response")
 		}
