8b7c24108045101e918192451a50d64354e66a1c
kris <kristen.f.jacobs@oracle.com>
2017-06-02 11:57:17 +0100

    Test commit 2

--- polibot.go
+++ polibot.go
@@ -10,7 +10,7 @@ func getResponseString() string {
 	acceptableResponse := []string{
 		"...Given this is an ongoing investigation, I am unable comment on specific details. However, what I can say is that, as a party, we will do everything in our power to ensure that this issue get resolved in both a quick and decisive manner.",
 		"What we need is a full and frank discussion about the issues that really matter",
-		"Lets be clear, nothing has changed in our manifesto, merely a clarification of the details",
+		"??????? Lets be clear, nothing has changed in our manifesto, merely a clarification of the details",
 		"What this country deserves is a strong and stable leadership",
 	}
 	return acceptableResponse[rand.Intn(len(acceptableResponse))]
